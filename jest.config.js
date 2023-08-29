3cfce6efc8b0b633be0be97bd95e82d5394d78d8
LunaSeki <LunaSeki@outlook.com>
2023-08-29 18:30:21 +0800

    fix: jest config ignore mocha test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
 	testEnvironment: "node",
 	moduleFileExtensions: ["ts", "js"],
 	testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
-
+	testPathIgnorePatterns: [
+		"/node_modules/", // 忽略 node_modules 文件夹下的测试
+		"/test/", // 忽略 test 文件夹及其子文件夹下的测试
+	],
 	transform: {
 		"^.+\\.tsx?$": [
 			"ts-jest",
